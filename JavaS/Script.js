5c1a3514953cd36c4b75c03c72db7e80e63a54cd
Aluno <Aluno@151ET1109650.catolica.ubec>
2024-04-09 10:36:40 -0300

    NavBar inteligente

--- JavaS/Script.js
+++ JavaS/Script.js
@@ -7,14 +7,39 @@ function copyToClipBoard() {
 let posX = window.scrollX;
 let posY = window.scrollY;
 
-let a = posX;
-let b = posY;
+let posXRef = posX;
+let posYref = posY;
+
+let mouseOn = false;
+
+const navbar = document.getElementById("NavBar");
 
 setInterval(() => {
-    console.clear()
+
+
     posX = window.scrollX;
     posY = window.scrollY;
 
-    console.log("X: " + posX);
-    console.log("Y: " + posY);
-}, 500);
+    if (posX !== posXRef | posY !== posYref) {
+        console.log("X: " + posX);
+        console.log("Y: " + posY);
+        posXRef = posX;
+        posYref = posY;
+    }
+    navbar.addEventListener("mouseover", ()=>{
+        console.log("Mouse on");
+        navbar.style.opacity = "1";
+        mouseOn = true;
+        setTimeout(()=>{
+            mouseOn = false;
+        }, 5000)
+    })
+    if(posY == 0 && !mouseOn){
+        navbar.style.opacity = "0";
+    } else {
+        navbar.style.opacity = "1";
+    }
+}, 800);
+
+
+
